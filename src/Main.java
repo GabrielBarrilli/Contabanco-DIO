95c5157cdd5fd06b4d28f633ab1d57018bacb011
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-04-24 16:19:22 -0300

    Correção de Bug ao digitar valor em scanner

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,18 @@ public class Main {
 
         System.out.println("Qual o número da agência?");
         int numero = scanner.nextInt();
-        System.out.println();
 
         System.out.println("Qual a agência?");
-        String agencia = scanner.nextLine();
-        System.out.println();
+        String agencia = scanner.next();
 
         System.out.println("Qual o nome do cliente?");
-        String nome = scanner.nextLine();
-        System.out.println();
+        String nome = scanner.next();
 
         System.out.println("Qual o saldo?");
         double saldo = scanner.nextDouble();
-        System.out.println();
 
         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco," +
-                " sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo
+                " sua agência é " + agencia + ", conta " + numero + " e seu saldo de " + saldo
                 + " já está disponível para saque");
     }
 }
